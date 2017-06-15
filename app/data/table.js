b9aff54fc8ecbf13d6585d12870112f68e05d57c
Kevin Wilson <kevinhn27@gmail.com>
2017-06-14 19:24:29 -0700

    first commit, html pages done, data done

---
+++ app/data/table.js
@@ -0,0 +1,17 @@
+var table =
+    [
+        {
+            "customerName": "kevin",
+            "phoneNumber": "9113294713947832",
+            "customerEmail": "kevin@kevin.com",
+            "customerID": "420"
+        },
+        {
+            "customerName": "Test",
+            "phoneNumber": "(123)456-7890",
+            "customerEmail": "Test", "customerID": "1",
+            "customerID": "test"
+        }
+    ]
+
+module.exports = table;
---
+++ app/data/wait.js
@@ -0,0 +1,17 @@
+var wait =
+    [
+        {
+            "customerName": "wait",
+            "phoneNumber": "9113294713947832",
+            "customerEmail": "kevin@kevin.com",
+            "customerID": "wait"
+        },
+        {
+            "customerName": "Test",
+            "phoneNumber": "(123)456-7890",
+            "customerEmail": "Test", "customerID": "1",
+            "customerID": "list"
+        }
+    ]
+
+module.exports = wait;
